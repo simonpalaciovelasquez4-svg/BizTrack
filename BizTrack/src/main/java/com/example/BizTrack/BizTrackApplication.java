8bb0b41871d191213b262a132f646b8ef0dae34e
yeral <yeralvalderrama2@gmail.com>
2026-08-28 06:09:09 -0500

    feat se crea proyecto

---
+++ BizTrack/src/main/java/com/example/BizTrack/BizTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.BizTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizTrackApplication.class, args);
+	}
+
+}
---
+++ BizTrack/src/test/java/com/example/BizTrack/BizTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BizTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
